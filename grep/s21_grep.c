5e70bc532cba738a37b63ec4b36a0c591b82ca46
Joramun Sasin <joramuns@student.21-school.ru>
2022-05-24 17:07:58 +0300

    Implemented REGEX search

--- grep/s21_grep.c
+++ grep/s21_grep.c
@@ -12,17 +12,20 @@
 #include <regex.h>
 
 int main(int argc, char *argv[]) {
-    if (argc > 1) {
+    if (argc > 2) {
 /* Read flags */
         flags grep_flags = {1, 1, 0, 0, 0, 0, 0, 0, 0};
         int i = check_flag(argv, argc, &grep_flags);
 /* Prioritize flags */
 //        priorities(&grep_flags);
 
+/* Read pattern */
+        char *pattern = argv[i++];
+        printf("\nPattern = %s\n", pattern);
+
 /* Output each file */
-        while (i <= argc && i != grep_flags.number) {
-            grep_output(grep_flags, argv[i - 1]);
-            i++;
+        while (i < argc && i != grep_flags.number) {
+            grep_output(grep_flags, argv[i++], pattern);
         }
 /* In case of no options called, throw error message */
     } else {
@@ -32,37 +35,53 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void grep_output(flags grep_flags, const char *filename) {
+void grep_output(flags grep_flags, const char *filename, const char *pattern) {
     FILE *fp = fopen(filename, "r");
 
     if (fp) {
-        int c = 0, len = 0;
-/* Get strings with line till the end of file */
-        while (c != -1) {
-            char *str = malloc(1 * sizeof(char));
-            c = fgetc(fp);
-            putchar(c);
-/* Get chars and relloc the size of array until \n */
-            while (c != 10 && c != -1) {
-                if (c != 10) {
-                    len++;
-                    str = realloc(str, sizeof(char) * (len + 1));
-                    str[len - 1] = (char)c;
-                    str[len] = '\0';
-                }
-                c = fgetc(fp);
-                putchar(c);
-            }
-/* Analyze the line and free it */
-            if (len > 0) {
-                printf("Line = %s\n", str);
+        regex_t regex;
+        regcomp(&regex, pattern, 0);
+        char *str = grep_getline(fp);
+        int line_number = 1;
+        while (str) {
+            if (!regexec(&regex, str, 0, NULL, 0)) {
+                printf("\n%d:Line = '%s'\n", line_number, str);
             }
             free(str);
-            str = NULL;
-            len = 0;
+            str = grep_getline(fp);
+            line_number++;
         }
-    } else {
-/* Error output */
+        regfree(&regex);
+    } else if (!grep_flags.s_flag) {
+/* Error output excluding -s flag (suppress error message) */
         fprintf(stderr, "grep: %s: %s\n", filename, strerror(errno));
     }
+}
+
+char *grep_getline(FILE *filepointer) {
+    int c = 0, len = 1;
+    char *line = calloc(len, sizeof(char));
+/* Get strings with line till the end of file */
+    while ((c = fgetc(filepointer)) != -1 && c != 10 && line) {
+/* Temp output */
+        putchar(c);
+/* Get chars and relloc the size of array until \n */
+        len++;
+        char *temp_line = realloc(line, sizeof(char) * (len));
+/* Realloc safely please! */
+        if (temp_line) {
+            line = temp_line;
+            line[len - 2] = (char)c;
+            line[len - 1] = '\0';
+        } else {
+            free(line);
+            line = NULL;
+        }
+    }
+/* End of file and empty line */
+    if (c == -1 && len == 1) {
+        free(line);
+        line = NULL;
+    }
+    return line;
 }
--- grep/s21_grep.h
+++ grep/s21_grep.h
@@ -10,6 +10,7 @@
 #include "../common/check_flag.h"
 #include <stdio.h>
 
-void grep_output(flags grep_flags, const char *filename);
+void grep_output(flags grep_flags, const char *filename, const char *pattern);
+char *grep_getline(FILE *filepointer);
 
 #endif  // SRC_GREP_S21_GREP_H_
