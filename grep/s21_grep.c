e6a38387fda8c5537cee09134afc5966f58a2042
Joramun Sasin <joramuns@student.21-school.ru>
2022-05-24 20:07:10 +0300

    Flags -i -v -c -l -n -s implemented with little compatibility

--- grep/s21_grep.c
+++ grep/s21_grep.c
@@ -14,22 +14,29 @@
 int main(int argc, char *argv[]) {
     if (argc > 2) {
 /* Read flags */
-        flags grep_flags = {1, 1, 0, 0, 0, 0, 0, 0, 0};
+        flags grep_flags = {2, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
         int i = check_flag(argv, argc, &grep_flags);
+        grep_flags.pars_pos = 1;  // temp null
 /* Prioritize flags */
 //        priorities(&grep_flags);
-
 /* Read pattern */
-        char *pattern = argv[i++];
-        printf("\nPattern = %s\n", pattern);
+        while (*argv[grep_flags.pars_pos] == '-' && grep_flags.pars_pos < argc) {
+            grep_flags.pars_pos++;
+        }
+        char *pattern = argv[grep_flags.pars_pos];
+        grep_flags.pars_pos++;
 
 /* Output each file */
-        while (i < argc && i != grep_flags.number) {
-            grep_output(grep_flags, argv[i++], pattern);
+        while (grep_flags.pars_pos < argc) {
+            if (*argv[grep_flags.pars_pos] == '-') {
+                grep_flags.pars_pos++;
+            } else {
+                grep_output(grep_flags, argv[grep_flags.pars_pos++], pattern);
+            }
         }
 /* In case of no options called, throw error message */
     } else {
-        fprintf(stderr, "usage: s21_grep [-eivclnhsfo] [file ...]");
+        fprintf(stderr, "usage: s21_grep [-eivclnhsfo] [pattern] [file ...]");
     }
 
     return 0;
@@ -37,19 +44,34 @@ int main(int argc, char *argv[]) {
 
 void grep_output(flags grep_flags, const char *filename, const char *pattern) {
     FILE *fp = fopen(filename, "r");
+    int reg_option = grep_flags.i_flag ? REG_ICASE : 0;
 
     if (fp) {
         regex_t regex;
-        regcomp(&regex, pattern, 0);
+        regcomp(&regex, pattern, reg_option);
         char *str = grep_getline(fp);
-        int line_number = 1;
+        int line_number = 1, line_count = 0;
         while (str) {
-            if (!regexec(&regex, str, 0, NULL, 0)) {
-                printf("\n%d:Line = '%s'\n", line_number, str);
+/* Print either matched line (if no -v flag) or other than matched line */
+            if (regexec(&regex, str, 0, NULL, 0) == grep_flags.v_flag) {
+/* With -n flag a number of line will be output */
+                if (!grep_flags.c_flag && !grep_flags.l_flag) {
+                    if (grep_flags.n_flag) {
+                        printf("%d:", line_number);
+                    }
+                    printf("%s\n", str);
+                }
+                    line_count++;
             }
             free(str);
             str = grep_getline(fp);
             line_number++;
+        }
+        if (grep_flags.c_flag) {
+            printf("%d\n", line_count);
+        }
+        if (grep_flags.l_flag && line_count > 0) {
+            printf("%s\n", filename);
         }
         regfree(&regex);
     } else if (!grep_flags.s_flag) {
@@ -63,8 +85,6 @@ char *grep_getline(FILE *filepointer) {
     char *line = calloc(len, sizeof(char));
 /* Get strings with line till the end of file */
     while ((c = fgetc(filepointer)) != -1 && c != 10 && line) {
-/* Temp output */
-        putchar(c);
 /* Get chars and relloc the size of array until \n */
         len++;
         char *temp_line = realloc(line, sizeof(char) * (len));
