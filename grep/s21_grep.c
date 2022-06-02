886b3ed58960178d5a1d6e4d9da3cd50854822a3
Joramun Sasin <joramuns@student.21-school.ru>
2022-06-02 14:23:44 +0300

    Refactored before pattern fix

--- grep/s21_grep.c
+++ grep/s21_grep.c
@@ -47,13 +47,8 @@ int main(int argc, char *argv[]) {
 void grep_output(flags grep_flags, const char *filename) {
     FILE *fp = fopen(filename, "r");
     if (fp) {
-        /* Used for sensibility in pattern case */
-        int reg_option = grep_flags.i_flag ? REG_ICASE : 0;
-        regex_t regex;
-        regcomp(&regex, grep_flags.pattern,  REG_EXTENDED|reg_option);
-        int line_count = in_search(fp, &regex, grep_flags, filename);
+        int line_count = in_search(fp, grep_flags, filename);
         af_search(grep_flags, filename, line_count);
-        regfree(&regex);
         fclose(fp);
     } else if (!grep_flags.s_flag) {
 /* Error output excluding -s flag (suppress error message) */
@@ -61,50 +56,59 @@ void grep_output(flags grep_flags, const char *filename) {
     }
 }
 
-int in_search(FILE *fp, regex_t *regex, flags a, const char *filename) {
+int in_search(FILE *fp, flags a, const char *filename) {
     char *str = grep_getline(fp);
     int line_number = 1, line_count = 0;
+/*          Used for sensibility of case in pattern     */
 
     while (str) {
-/* Print either matched line (if no -v flag) or other than matched line */
-        regmatch_t reg_res[5];
-        if (regexec(regex, str, 5, reg_res, 0) == a.v_flag) {
-/* With -n flag a number of line will be output */
-            if (!a.c_flag && !a.l_flag) {
-                if (a.num_files && !a.h_flag) {
-                    printf("%s:", filename);
-                }
-                if (a.n_flag) {
-                    printf("%d:", line_number);
-                }
-                if (a.o_flag && !a.v_flag) {
-                    char *newstr = str;
-                    while (!regexec(regex, newstr, 5, reg_res, 0)) {
-                        if (reg_res[0].rm_eo == 0 && reg_res[0].rm_so == 0) {
-                            break;
-                        }
-                        regoff_t len = reg_res[0].rm_eo - reg_res[0].rm_so;
-                        newstr += reg_res[0].rm_so;
-                        char *substr = strndup(newstr, len);
-                        if (substr) {
-                            printf("%s\n", substr);
-                            free(substr);
-                        }
-                        newstr += len;
-                    }
-                } else {
-                    printf("%s\n", str);
-                }
-            }
-/* Iterate line counter if not -l flag, otherwise - bool */
-            a.l_flag ? line_count = 1 : line_count++;
-        }
+        search(a, filename, &line_count, line_number, str);
         free(str);
         str = grep_getline(fp);
         line_number++;
     }
 
     return line_count;
+}
+
+void search(const flags a, const char *filename, int *line_count, int line_number, char *str) {
+    int reg_option = a.i_flag ? REG_ICASE : 0;
+    regex_t regex;
+    regcomp(&regex, a.pattern,  REG_EXTENDED|reg_option);
+    /* Print either matched line (if no -v flag) or other than matched line */
+    regmatch_t reg_res[5];
+    if (regexec(&regex, str, 5, reg_res, 0) == a.v_flag) {
+        /* With -n flag a number of line will be output */
+        if (!a.c_flag && !a.l_flag) {
+            if (a.num_files && !a.h_flag) {
+                printf("%s:", filename);
+            }
+            if (a.n_flag) {
+                printf("%d:", line_number);
+            }
+            if (a.o_flag && !a.v_flag) {
+                char *newstr = str;
+                while (!regexec(&regex, newstr, 5, reg_res, 0)) {
+                    if (reg_res[0].rm_eo == 0 && reg_res[0].rm_so == 0) {
+                        break;
+                    }
+                    regoff_t len = reg_res[0].rm_eo - reg_res[0].rm_so;
+                    newstr += reg_res[0].rm_so;
+                    char *substr = strndup(newstr, len);
+                    if (substr) {
+                        printf("%s\n", substr);
+                        free(substr);
+                    }
+                    newstr += len;
+                }
+            } else {
+                printf("%s\n", str);
+            }
+        }
+        /* Iterate line counter if not -l flag, otherwise - bool */
+        a.l_flag ? *line_count = 1 : (*line_count)++;
+    }
+    regfree(&regex);
 }
 
 void af_search(flags a, const char *filename, int line_count) {
--- grep/s21_grep.h
+++ grep/s21_grep.h
@@ -12,7 +12,8 @@
 #include <regex.h>
 
 void    grep_output(flags grep_flags, const char *filename);
-int     in_search(FILE *fp, regex_t *regex, flags a, const char *filename);
+int     in_search(FILE *fp, flags a, const char *filename);
+void    search(const flags a, const char *filename, int *line_count, int line_number, char *str);
 void    af_search(flags a, const char *filename, int line_count);
 
 #endif  // SRC_GREP_S21_GREP_H_
