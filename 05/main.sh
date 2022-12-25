b7dce05aedcce85e3d47e4dd6f05440b2461943d
root <root@859971-cs30258.tmweb.ru>
2022-12-25 03:25:24 +0300

    feat(q5): more lines working, found bugs in previous task

--- 05/main.sh
+++ 05/main.sh
@@ -3,14 +3,18 @@
 time_start=$(date +"%s.%N")
 total_num_folders=$[$(sudo find $1 -type d | wc -l)-1]
 top5_folders=$(sudo du -chd1 $1 | head -n -2 | head -5 | sort -hr | awk '{print "- "$2", "$1}' | nl)
-total_num_files=$(sudo find $1 -type f | wc -l)
-#
-#
-#
-#
-#
-#
-#top10_files=
+sudo find $1 -type f > temp
+#total_num_files=$(sudo find $1 -type f | wc -l)
+total_num_files=$(cat temp | wc -l)
+conf_num=$(grep ".conf" temp | wc -l)
+log_num=$(grep ".log" temp | wc -l)
+sudo file -f temp | awk '{$1 = ""; print $0}' > typetemp
+text_num=$(grep "text$" typetemp | wc -l)
+exec_num=$(grep "executable" typetemp | wc -l)
+archive_num=$(grep "compressed" typetemp | wc -l)
+link_num=$(sudo find $1 -type l | wc -l)
+sudo find $1 -type f -exec du -h {} + | awk '{print $2" "$1}' > sizetemp
+top10_files=$(paste sizetemp typetemp -d " " | sort -rh | head -10)
 #
 
 echo "Total number of folders (including all nested ones) = $total_num_folders"
@@ -18,22 +22,17 @@ echo "TOP 5 folders of maximum size arranged in descending order (path and size)
 echo "$top5_folders"
 echo "Total number of files = $total_num_files"
 echo "Number of:"
-echo "Configuration files (with the .conf extension) = "
-echo "Text files = "
-echo "Executable files = "
-echo "Log files (with the extension .log) = "
-echo "Archive files = "
-echo "Symbolic links = "
-#
-#
-#
-#
-#
-#
+echo "Configuration files (with the .conf extension) = $conf_num"
+echo "Text files = $text_num"
+echo "Executable files = $exec_num"
+echo "Log files (with the extension .log) = $log_num"
+echo "Archive files = $archive_num"
+echo "Symbolic links = $link_num"
 echo "TOP 10 files of maximum size arranged in descending order (path, size and type):"
-#echo "$top10_files"
+echo "$top10_files"
 echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file)"
 #
+#sudo rm temp
 
 time_end=$(date +"%s.%N")
 total_time=$(echo "$time_end - $time_start" | bc | awk '{printf "%.2f", $1}')
