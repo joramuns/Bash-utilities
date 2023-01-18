dfbce02a6cfd234a7120e75936c1ad78a95c19f3
joramuns_ws1 <joramuns@student.21-school.ru>
2023-01-18 20:42:50 +0300

    fix(q5): fix top5 size folders, add top10 size executables

--- 05/main.sh
+++ 05/main.sh
@@ -2,9 +2,8 @@
 
 time_start=$(date +"%s.%N")
 total_num_folders=$[$(sudo find $1 -type d | wc -l)-1]
-top5_folders=$(sudo du -chd1 $1 | head -n -2 | head -5 | sort -hr | awk '{print "- "$2", "$1}' | nl)
+top5_folders=$(sudo du -chd1 $1 | sort -hr | awk 'NR>2' | head -5 | awk '{print $2", "$1}' | nl -s " - " -n ln -w 1)
 sudo find $1 -type f > temp
-#total_num_files=$(sudo find $1 -type f | wc -l)
 total_num_files=$(cat temp | wc -l)
 conf_num=$(grep ".conf" temp | wc -l)
 log_num=$(grep ".log" temp | wc -l)
@@ -13,9 +12,12 @@ text_num=$(grep "text$" typetemp | wc -l)
 exec_num=$(grep "executable" typetemp | wc -l)
 archive_num=$(grep "compressed" typetemp | wc -l)
 link_num=$(sudo find $1 -type l | wc -l)
-sudo find $1 -type f -exec du -h {} + | awk '{print $2" "$1}' > sizetemp
-top10_files=$(paste sizetemp typetemp -d " " | sort -rh | head -10)
-#
+sudo find $1 -type f -exec du -h {} + | awk '{print $2", "$1","}' > sizetemp
+sudo find $1 -type f -executable -exec du -h {} + | awk '{print $2", "$1","}' > execsizetemp
+sudo find $1 -type f -executable -exec md5sum {} + | awk '{print $1}' > execmd5sum
+
+top10_files=$(paste sizetemp typetemp -d " " | sort -rh | head -10 | nl -s " - " -n ln -w 1)
+top10_exec_files=$(paste execsizetemp execmd5sum -d " " | sort -rh | head -10 | nl -s " - " -n ln -w 1)
 
 echo "Total number of folders (including all nested ones) = $total_num_folders"
 echo "TOP 5 folders of maximum size arranged in descending order (path and size):"
@@ -31,7 +33,7 @@ echo "Symbolic links = $link_num"
 echo "TOP 10 files of maximum size arranged in descending order (path, size and type):"
 echo "$top10_files"
 echo "TOP 10 executable files of the maximum size arranged in descending order (path, size and MD5 hash of file)"
-#
+echo "$top10_exec_files"
 #sudo rm temp
 
 time_end=$(date +"%s.%N")
