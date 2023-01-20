e33d69d74ab0a13c5122a0c4d128fa7e0c18f01f
joramuns_ws1 <joramuns@student.21-school.ru>
2023-01-20 21:14:44 +0300

    fix(q5): default colors fix

--- 04/main.sh
+++ 04/main.sh
@@ -2,28 +2,28 @@
 
 source colorscheme.conf
 
-if [[ ($1 != [1-6]) ]]
+if [[ 1 -le $column1_background && $column1_background -le 6 ]]
 then
 	out1=$column1_background
 else
-	out1=$1
+	out1=7
 fi
-if [[ ($2 != [1-6]) ]]
+if [[ 1 -le $column1_font_color && $column1_font_color -le 6 ]]
 then
 	out2=$column1_font_color
 else
-	out2=$2
+	out2=7
 fi
-if [[ ($3 != [1-6]) ]]
+if [[ 1 -le $column2_background && $column2_background -le 6 ]]
 then
 	out3=$column2_background
 else
-	out3=$3
+	out3=7
 fi
-if [[ ($4 != [1-6]) ]]
+if [[ 1 -le $column2_font_color && $column2_font_color -le 6 ]]
 then
 	out4=$column2_font_color
 else
-	out4=$4
+	out4=7
 fi
 	source colored_output.sh
