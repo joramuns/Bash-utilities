3e99416734f39cd1254d5ac3ba4a55b653674890
Joramuns il-C3 <illusion@c3.vb>
2023-01-30 17:47:09 +0000

    feat(q1): log file

--- 01/main.sh
+++ 01/main.sh
@@ -10,34 +10,9 @@
 source create.sh
 source generate_file.sh
 source generate_folder.sh
+source check_input.sh
 
-space_avail=$(df / | tail -1 | awk '{print $4}')
-
-# if [[ $# -ne 6 ]]; then
-# 	echo "Not enough arguments!"
-#     exit 1
-# else
-	source check_input.sh
-# fi
-
-if [[ $check_counter -ne 6 ]]; then
-    exit 1
+if [[ $check_counter -eq 6 ]]; then
+    start_create $1 $2 $3 $4 $5 $6
 fi
 
-folder_init_pattern $3
-for (( m=0; m<$2; m++ ))
-do
-    generate_folder
-    mkdir -p $1/$foldername
-    file_init_pattern $5
-    for (( p=0; p<$4; p++ ))
-    do
-        generate_filename
-        fallocate $1$foldername/$filename -l $6
-        space_left=$(df -m / | tail -1 | awk '{print $4}')
-        if (( space_left < 1024 )); then
-            echo "Less than 1GB of free space remaining!"
-            exit 99
-        fi
-    done
-done
