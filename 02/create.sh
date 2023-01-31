9acc6a7bca978382508dd6f339fd920d75d61699
Joramuns il-C3 <illusion@c3.vb>
2023-01-31 14:23:29 +0000

    feat(q3): get your own path

---
+++ 02/create.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+function start_create () {
+    folder_init_pattern $1
+    for (( m=0; m<$folder_num; m++ ))
+    do
+        get_path
+        generate_folder
+        echo $path_21
+        mkdir -p $path_21/$foldername
+        file_init_pattern $2
+        file_num=$(( $RANDOM % 1000 + 1 ))
+        for (( p=0; p<$file_num; p++ ))
+        do
+            generate_filename
+            fallocate $path_21/$foldername/$filename -l $3
+            echo "$(realpath $path_21/$foldername/$filename) $(date +%d.%m.%y) $3" >> log.txt
+            space_left=$(df -m / | tail -1 | awk '{print $4}')
+            if (( space_left < 1024 )); then
+                echo "Less than 1GB of free space remaining!"
+                exit 99
+            fi
+        done
+    done
+}
+
+function get_path () {
+    path_21=./test
+}
