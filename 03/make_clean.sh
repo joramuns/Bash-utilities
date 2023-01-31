2be3f7af06812963c78fd5853fbdbbf6baa76d6c
Joramuns il-C3 <illusion@c3.vb>
2023-01-31 19:09:24 +0000

    feat(q2): clean by find with all possible name combinations

--- 03/make_clean.sh
+++ 03/make_clean.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 function log_clean () {
-    echo "Cleaning in process!"
+    echo "Cleaning by logfile in process!"
     cat $1 | head -n-1 | awk 'NF==2{print $1}' | xargs rm -rf
+}
+
+function mask_file_clean () {
+    echo "Cleaning by file mask in process!"
+    pattern=$(echo $1 | awk -F "_" '{print $1}')
+    appendix=$(echo $1 | awk -F "_" '{print $2}')
+    folder_init_pattern $pattern
+    while (true)
+    do
+        generate_folder
+        find ~ -name $foldername -exec rm -rf {} +
+    done
 }
