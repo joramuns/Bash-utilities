8fa6a0d4bd676c2a499d6473536329d68ecf2a1a
Joramuns il-C3 <illusion@c3.vb>
2023-01-31 19:08:48 +0000

    feat(q2): option 3 handle

--- 03/clean_options.sh
+++ 03/clean_options.sh
@@ -9,3 +9,18 @@ function log_clean_opt () {
         exit 1
     fi
 }
+
+function date_time_opt () {
+    echo "Not implemented"
+}
+
+function mask_file_opt () {
+    read -p "Enter foldername mask: " -a maskfile
+    re='^[a-zA-Z]{1,7}[_][0-9]{6}$'
+    if [[ $maskfile =~ $re ]]; then
+        mask_file_clean $maskfile
+    else
+        echo "Wrong folder name mask!"
+        exit 2
+    fi
+}
