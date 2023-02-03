cd0fcdb53807cb9b6fe2af7b2ca650a99b3de34d
Joramuns il-C3 <illusion@c3.vb>
2023-02-03 16:00:52 +0000

    feat(q4): write to log to file

--- 04/main.sh
+++ 04/main.sh
@@ -2,11 +2,12 @@
 
 source line.sh
 
-num_files=1
-num_lines=$(get_random 100 1000)
+num_files=5
 
 for (( m_i=0; m_i < $num_files; m_i++ ))
 do
-    make_log $num_lines
+    filename="nginx"$m_i".log"
+    num_lines=$(get_random 100 1000)
+    make_log $num_lines > $filename
 done
 
