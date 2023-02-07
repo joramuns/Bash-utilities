8eb40cc21c0035cfadbbe2e9ec4e280957f98562
Joramuns il-C3 <illusion@c3.vb>
2023-02-07 15:02:51 +0000

    fix(q5): reduce scope of variable codes

--- 05/options.sh
+++ 05/options.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 files=$(find . -name "*.log")
-codes=($(./get_response_codes.awk $files))
 
 function opt_sort_response () {
+    codes=($(./get_response_codes.awk $files))
     for (( io_response=0; io_response < ${#codes[@]}; io_response++))
     do
         ./response.awk -v i_code=${codes[$io_response]} $files
