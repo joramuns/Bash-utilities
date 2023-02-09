8f07af24fbd7fd8a754b5f83d92ca5ec51549571
Joramuns il-C3 <illusion@c3.vb>
2023-02-09 18:28:26 +0000

    chore(q9): bash script generating html file

---
+++ 09/main.sh
@@ -0,0 +1,18 @@
+while (true)
+do
+    info_path="/var/www/html/metrics/data.html"
+    cpu_load=$(cat /proc/loadavg | awk '{print $1}')
+    free_space=$(df -m / | tail -1 | awk '{print $4}')
+    free_ram=$(free -m | awk '/Mem/ {print $4}')
+    echo "# HELP s21_cpu_load CPU usage" > $info_path
+    echo "# TYPE s21_cpu_load gauge" >> $info_path
+    echo "s21_cpu_load $cpu_load" >> $info_path
+    echo "# HELP s21_free_space Free space" >> $info_path
+    echo "# TYPE s21_free_space gauge" >> $info_path
+    echo "s21_free_space $free_space" >> $info_path
+    echo "# HELP s21_ram_available RAM Available" >> $info_path
+    echo "# TYPE s21_ram_available gauge" >> $info_path
+    echo "s21_ram_available $free_ram" >> $info_path
+    echo "Data stored"
+    sleep 3
+done
