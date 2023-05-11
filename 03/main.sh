421ff4134dc14a29e3fa0a042fae5e372966a1a8
joramuns <joramuns@student.21-school.ru>
2023-05-11 22:38:15 +0300

    chore: rebuild project architecture from educational to expositional

--- 03/main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ ($# -ne 4) || !($1 =~ ^[1-6]$) || !($2 =~ ^[1-6]$) || !($3 =~ ^[1-6]$) || !($4 =~ ^[1-6]$) || ($1 == $2) || ($3 == $4) ]]
-then
-	echo "Incorrect color info"
-else
-	source colored_output.sh
-fi
